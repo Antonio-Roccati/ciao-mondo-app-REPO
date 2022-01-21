25b50f2da680fee23960b8a5b8f2620245416561
Andrea <98151079+Antonio-Roccati@users.noreply.github.com>
2022-01-21 10:40:19 +0100

    fatto ciao mondo

---
+++ Ciao mondo/Ciao mondo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ciao_mondo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
