727b4474e6f6f32a69b72e2bed8811b88850ccd2
Andrea <98151079+Antonio-Roccati@users.noreply.github.com>
2022-01-28 09:49:53 +0100

    cambiato
    mi ero rotto

---
+++ console.app/console.app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("salve a tutti , signori e signore, adesso andatevene tutti via ");
+        }
+    }
+}
